d9ab91aa5697e65df1a7b352f6eb6e6ac05239c4
djasmineangela <48932763+djasmineangela@users.noreply.github.com>
2019-03-26 08:31:37 +0800

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,14 @@
+
+import java.util.Scanner;
+
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        Scanner jas = new Scanner (System.in);
+        int n = jas.nextInt();
+        
+        for(int i=0;  i<n; i++){
+            System.out.println("Hello World");
+        }
+    }
+}
